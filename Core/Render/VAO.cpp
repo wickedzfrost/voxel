c6046489bb1a9f887ccc61ba49f0feb6f62d6968
Frost <wickedzfrost@gmail.com>
2025-07-08 13:22:46 +0800

    Create VAO Wrapper

---
+++ Core/Render/VAO.cpp
@@ -0,0 +1,33 @@
+#include "VAO.h"
+
+VAO::VAO()
+{
+    glGenVertexArrays(1, &m_id);
+}
+
+VAO::~VAO()
+{
+    glDeleteVertexArrays(1, &m_id);
+}
+
+void VAO::LinkVBO(VBO& vbo, GLuint layout, GLint size, GLenum type, GLsizei stride, std::size_t offset) 
+{
+    Bind();
+    vbo.Bind();
+
+    glVertexAttribPointer(layout, size, type, GL_FALSE, stride, reinterpret_cast<const void*>(offset));
+    glEnableVertexAttribArray(layout);
+
+    vbo.Unbind();
+    Unbind();
+}
+
+void VAO::Bind()
+{
+    glBindVertexArray(m_id);
+}
+
+void VAO::Unbind()
+{
+    glBindVertexArray(0);
+}
--- Core/Render/VAO.h
+++ Core/Render/VAO.h
@@ -1,6 +1,21 @@
 #ifndef VAO_H
 #define VAO_H
 
+#include <glad/glad.h>
+#include "VBO.h"
 
+class VAO
+{
+public:
+    GLuint m_id{};
+
+    VAO();
+
+    ~VAO();
+
+    void LinkVBO(VBO& vbo, GLuint layout, GLint size, GLenum type, GLsizei stride, std::size_t offset);
+    void Bind();
+    void Unbind();
+};
 
 #endif // !VAO_H
